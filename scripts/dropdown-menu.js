4d129de6ab23fb0b55269bd9192f6765b85ad9cc
muhammad_abbas <alikausar157@gmail.com>
2023-12-11 21:46:43 -0800

    Setting up repo

---
+++ scripts/dropdown-menu.js
@@ -0,0 +1,48 @@
+// Responsive Menu - Dropdown
+const body = document.body;
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.getElementById('mobile-nav');
+
+// Below code for preventing nav from animating on 
+// browser re-size modified from code found at
+// this stackoverflow question and answer:
+// https://goo.gl/6s3pAZ
+
+
+btnMenu.addEventListener('click', openMenu);
+// Prevents the focus state from activating
+btnMenu.addEventListener('mousedown', function(e){
+    e.preventDefault();
+});
+
+function openMenu(){
+    body.classList.toggle('show');
+	nav.classList.add('activated');
+}
+
+// Media Query Event Listener
+// removes the "activated" class from the nav
+// when user resizes browser
+
+// Create a media query list using
+// matchMedia
+const mql = window.matchMedia('(min-width: 560px)');
+
+// Add a Media Query Listener to the 
+// above media query list
+mql.addListener(removeTransition);
+
+// Function to remove transition from the nav
+// when user resizes browser to desktop
+// screen width wider than 560px
+
+function removeTransition(e){
+	// e -> is the event object
+	// e.matches -> stores a true false value
+	// depending if the media query
+	// list set above matches or not
+	if(e.matches){
+		body.classList.remove('show');
+		nav.classList.remove('activated');
+	}
+}
